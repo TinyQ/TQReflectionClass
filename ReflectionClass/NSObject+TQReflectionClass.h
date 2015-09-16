fc37a73f130f1a36c91d69e2ee27ef1c1248e989
qfu <tinyqf@gmail.com>
2015-09-16 22:05:50 +0800

    add NSObject+TQReflectionClass files

---
+++ ReflectionClass/NSObject+TQReflectionClass.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+TQReflectionClass.h
+//  TQReflectionClass
+//
+//  Created by qfu on 9/16/15.
+//  Copyright (c) 2015 qfu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (TQReflectionClass)
+
+@end
---
+++ ReflectionClass/NSObject+TQReflectionClass.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+TQReflectionClass.m
+//  TQReflectionClass
+//
+//  Created by qfu on 9/16/15.
+//  Copyright (c) 2015 qfu. All rights reserved.
+//
+
+#import "NSObject+TQReflectionClass.h"
+
+@implementation NSObject (TQReflectionClass)
+
+@end
